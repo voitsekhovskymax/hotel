ab8810c52647c70304b53d6897e5b63394cdcec5
voitsekhovskymax <voitsekhovskymax@gmail.com>
2018-12-24 17:59:33 +0200

    Отчет
    - модалка для liqpay

--- pages/reservations/_id.vue
+++ pages/reservations/_id.vue
@@ -56,21 +56,31 @@
 
             <v-text-field label="Скидка" v-model="response.orderRoom.discount"></v-text-field>
 
-            <v-text-field label="Количество дней предоплаты" v-model="response.orderRoom.col_prepaid_days"></v-text-field>
+            <v-text-field label="Количество дней предоплаты"
+                          v-model="response.orderRoom.col_prepaid_days"></v-text-field>
 
             <v-text-field label="Сумма предоплаты" v-model="response.orderRoom.sum_prepaid"></v-text-field>
+            <v-layout wrap>
 
-            <v-text-field label="Получена предоплата" v-model="response.orderRoom.was_prepaid"></v-text-field>
+              <v-text-field label="Получена предоплата" v-model="response.orderRoom.was_prepaid"></v-text-field>
+              <v-tooltip top>
+                <v-btn slot="activator" flat icon color="success" @click="dialogMail = true">
+                  <v-icon>mail_outline</v-icon>
+                </v-btn>
+                <span>Отправить письмо</span>
+              </v-tooltip>
+
+            </v-layout>
 
             <v-text-field label="Доплата" v-model="response.orderRoom.was_payed"></v-text-field>
 
             <v-text-field label="Уплачено всего" v-model="response.orderRoom.sum_payed"></v-text-field>
 
-            <v-text-field label="Способ оплаты" v-model="response.orderRoom.payment_type.name"
-                          :hint="response.orderRoom.payment_type.info"></v-text-field>
+            <!--<v-text-field label="Способ оплаты" v-model="response.orderRoom.payment_type.name"-->
+            <!--:hint="response.orderRoom.payment_type.info"></v-text-field>-->
 
-            <v-text-field label="Статус номера" v-model="response.orderRoom.room_status.name"
-                          :hint="response.orderRoom.room_status.info"></v-text-field>
+            <!--<v-text-field label="Статус номера" v-model="response.orderRoom.room_status.name"-->
+            <!--:hint="response.orderRoom.room_status.info"></v-text-field>-->
 
           </v-flex>
 
@@ -113,7 +123,7 @@
               </v-card>
             </template>
 
-            <template >
+            <template>
               <v-card color="grey lighten-3 mt-2">
 
                 <v-card-title>Трансфер на выезд</v-card-title>
@@ -135,7 +145,191 @@
 
 
       </v-card-text>
+      <v-card-actions>
+        <v-btn color="success">Сохранить</v-btn>
+        <v-btn flat color="error">Удалить</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="liqpay_btn" @click="dialogLiqpay=true"></v-btn>
+      </v-card-actions>
     </v-card>
+
+    <v-dialog v-model="dialogMail">
+      <v-card>
+        <v-layout wrap>
+          <v-flex sm6 md6 xs12>
+            <v-toolbar class="no-shadow">
+              <v-toolbar-title>Отправка письма</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+          </v-flex>
+
+          <v-flex sm6 md6 xs12>
+            <v-toolbar class="no-shadow">
+              <v-spacer></v-spacer>
+              <v-btn icon @click="dialogMail = false">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-toolbar>
+          </v-flex>
+        </v-layout>
+        <v-card-text>
+
+          <v-text-field label="Почта" v-model="response.client.email"></v-text-field>
+
+          <v-layout align-center justify-space-around row fill-height>
+            <v-select
+              label="Название письма"
+              hint="Выберите письмо из списка, и его содержимое добавится"
+              v-model="mail_select"
+              :items="messages"
+              item-text="title"
+              return-object
+
+            ></v-select>
+            <v-btn flat icon @click="deleteMailSelect">
+              <v-icon>delete</v-icon>
+            </v-btn>
+          </v-layout>
+
+          <v-textarea
+            v-model="mail_select.info"
+            auto-grow
+            label="Содержимое письма"
+            rows="1"
+          ></v-textarea>
+
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="success"
+            @click="sendMail"
+          >
+            Отправить
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+    <v-dialog v-model="dialogLiqpay" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card>
+        <v-layout wrap>
+          <v-flex sm6 md6 xs12>
+            <v-toolbar class="no-shadow">
+              <v-toolbar-title>Liqpay</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+          </v-flex>
+
+          <v-flex sm6 md6 xs12>
+            <v-toolbar class="no-shadow">
+              <v-spacer></v-spacer>
+              <v-btn icon @click="dialogLiqpay = false">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-toolbar>
+          </v-flex>
+        </v-layout>
+        <v-card-text>
+          <v-tabs
+            slot="extension"
+            v-model="currentTab"
+            color="transparent"
+            fixed-tabs
+            slider-color="success"
+          >
+            <v-tab href="#paid"> Liqpay предоплата</v-tab>
+            <v-tab href="#prepaid"> Liqpay оплата</v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="currentTab">
+            <v-tab-item id="paid">
+              <v-card>
+                <v-card-text>
+                  <v-layout wrap class="card-content ">
+                    <v-flex sm6 md6 xs12>
+                      <v-text-field label="Почта" v-model="response.client.email"></v-text-field>
+                      <v-text-field label="Сумма предоплаты" v-model="response.orderRoom.sum_prepaid"></v-text-field>
+                      <v-text-field label="order_id" v-model="response.orderRoom.order_num" readonly></v-text-field>
+                      <v-btn color="success" small>
+                        Отправить на LIQPAY
+                      </v-btn>
+                      <v-btn flat color="success" small>
+                        Перезапросить INVOICE
+                      </v-btn>
+                    </v-flex>
+                    <v-flex sm6 md6 xs12>
+
+                      <v-btn flat color="success" small>
+                        Получить статус
+                      </v-btn>
+
+                      <div class="table_statuses">
+                        <v-data-table
+                          :headers="liqpay_paid_headers"
+                          :items="liqpay_paid_statuses"
+                          hide-actions
+                          class="elevation-1"
+                        >
+                          <template slot="items" slot-scope="props">
+                            <td>{{ props.item.key }}</td>
+                            <td class="text-xs-right">{{ props.item.value }}</td>
+                          </template>
+                        </v-data-table>
+                      </div>
+
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+
+            <v-tab-item id="prepaid">
+              <v-card>
+                <v-card-text>
+                  <v-layout wrap class="card-content ">
+                    <v-flex sm6 md6 xs12>
+                      <v-text-field label="Почта" v-model="response.client.email"></v-text-field>
+                      <v-text-field label="Сумма предоплаты" v-model="response.orderRoom.sum_prepaid"></v-text-field>
+                      <v-text-field label="order_id" v-model="response.orderRoom.order_num" readonly></v-text-field>
+                      <v-btn color="success" small>
+                        Отправить на LIQPAY
+                      </v-btn>
+                      <v-btn flat color="success" small>
+                        Перезапросить INVOICE
+                      </v-btn>
+                    </v-flex>
+                    <v-flex sm6 md6 xs12>
+
+                      <v-btn flat color="success" small>
+                        Получить статус
+                      </v-btn>
+
+                      <div class="table_statuses">
+                        <v-data-table
+                          :headers="liqpay_paid_headers"
+                          :items="liqpay_paid_statuses"
+                          hide-actions
+                          class="elevation-1"
+                        >
+                          <template slot="items" slot-scope="props">
+                            <td>{{ props.item.key }}</td>
+                            <td class="text-xs-right">{{ props.item.value }}</td>
+                          </template>
+                        </v-data-table>
+                      </div>
+
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card-text>
+
+      </v-card>
+    </v-dialog>
+
   </div>
 
 </template>
@@ -144,6 +338,45 @@
   export default {
     data() {
       return {
+        liqpay_paid_headers: [
+          {
+            text: 'Ключ',
+            align: 'left',
+            sortable: false,
+            value: 'key'
+          },
+          {
+            text: 'Значение',
+            align: 'right',
+            sortable: false,
+            value: 'value'
+          },
+        ],
+        liqpay_paid_statuses: [{
+          key: 'code',
+          value: 'Платеж не найден'
+        }],
+        liqpay_prepaid_headers: [
+          {
+            text: 'Ключ',
+            align: 'left',
+            sortable: false,
+            value: 'key'
+          },
+          {
+            text: 'Значение',
+            align: 'right',
+            sortable: false,
+            value: 'value'
+          },
+        ],
+        liqpay_prepaid_statuses: [{
+          key: 'code',
+          value: 'Платеж не найден'
+        }],
+        currentTab: 'paid',
+        dialogLiqpay: false,
+        dialogMail: false,
         response: {
           "orderRoom": {
             "id": null,
@@ -244,20 +477,37 @@
           "liq_prepaid": null,
           "liq_paid": null
         },
-        params_id: this.$route.params.id
+        params_id: this.$route.params.id,
+        mail_select: {
+          info: null
+        },
+        messages: null,
+
       };
     },
     mounted() {
-      console.log(this.$route.params.id );
-      // this.axios.get('orders/' + this.$route.params.id + '/edit').then((response) => {
-      //   console.log(response);
-      //   this.response = response.data;
-      // });
+      console.log(this.$route.params.id);
+      this.axios.get('orders/' + this.$route.params.id + '/edit').then((response) => {
+        console.log(response);
+        this.response = response.data;
+      });
+      this.axios.get('messages').then((response) => {
+        this.messages = response.data.messages;
+      })
+
     },
     methods: {
+      sendMail() {
+
+      },
       routerBack() {
         this.$router.go(-1);
-      }
+      },
+      deleteMailSelect() {
+        this.mail_select = {
+          info: null
+        };
+      },
     }
   }
 </script>
