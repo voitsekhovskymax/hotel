fd246da7c6afb7d4e5861de9a90604c29db59cde
voitsekhovskymax <voitsekhovskymax@gmail.com>
2018-12-27 15:19:00 +0200

    Отчет
    Переписал меню. так проще передавать параметры в пункты меню

---
+++ components/NavbarList.vue
@@ -0,0 +1,329 @@
+<template>
+  <v-list dense>
+    <v-list-tile>
+      <nuxt-link to="/" class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Главная
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+    <v-divider/>
+    <v-list-group v-model="menu_models.notifications"
+                  :prepend-icon="menu_models.notifications ? 'keyboard_arrow_up' : 'notifications'">
+      <v-list-tile slot="activator">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Заявки
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <nuxt-link :to="{name: 'requests'}" class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Новые заявки
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link :to="{name: 'reservations'}" class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Все брони
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+    </v-list-group>
+
+
+    <v-list-tile>
+      <nuxt-link :to="{name: 'prices'}" class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>attach_money</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Цены на номера
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+
+    <v-list-tile>
+      <nuxt-link :to="{name: 'queue'}" class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>list</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Очередь
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+
+    <v-list-group v-model="menu_models.calendar_view_day"
+                  :prepend-icon="menu_models.calendar_view_day ? 'keyboard_arrow_up' : 'calendar_view_day'">
+      <v-list-tile slot="activator">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Отчеты
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'transfer-begin_date-end_date', params: {begin_date: begin_date, end_date: end_date}}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>directions_car</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Трансфер
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'parking-begin_date-end_date', params: {begin_date: begin_date, end_date: end_date}}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>local_parking</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Паркоместа
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'prepaid-begin_date-end_date', params: {begin_date: begin_date, end_date: end_date}}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>donut_large</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Статистика предоплат
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'payment-begin_date-end_date', params: {begin_date: begin_date, end_date: end_date}}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>donut_small</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Статистика оплат
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'clients-page', params: { page: 1}}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>supervised_user_circle</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Клиенты
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+    </v-list-group>
+
+    <v-list-tile>
+      <nuxt-link
+        :to="{ name: 'messages'}"
+        class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>mail_outline</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Рассылка писем
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+
+
+    <v-list-group v-model="menu_models.settings_applications"
+                  :prepend-icon="menu_models.settings_applications ? 'keyboard_arrow_up' : 'settings_applications'">
+      <v-list-tile slot="activator">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Параметры приложения
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'users'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>person</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Пользователи
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'payment_types'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>local_grocery_store</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Тип оплаты
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'room_types'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Тип номеров
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'room_statuses'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>done_all</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Статус номеров
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'rooms'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>local_hotel</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Все номера
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+      <v-list-tile>
+        <nuxt-link
+          :to="{ name: 'backup'}"
+          class="navigation-link">
+          <v-list-tile-action>
+            <v-icon>backup</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Восстановление данных
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </nuxt-link>
+      </v-list-tile>
+
+    </v-list-group>
+
+    <v-list-tile>
+      <nuxt-link
+        :to="{ name: 'settings'}"
+        class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>settings</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Настройки
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+    <v-divider></v-divider>
+    <v-list-tile>
+      <nuxt-link
+        :to="{ name: 'logout'}"
+        class="navigation-link">
+        <v-list-tile-action>
+          <v-icon>exit_to_app</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Выйти
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </nuxt-link>
+    </v-list-tile>
+  </v-list>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        begin_date: new Date().getFullYear() + '-01-01',
+        end_date: new Date().toISOString().substr(0, 10),
+        menu_models: {
+          notifications: false,
+          calendar_view_day: false,
+          settings_applications: false,
+        },
+      }
+    }
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -23,64 +23,7 @@
     </notifications>
 
     <v-navigation-drawer v-model="drawer" clipped fixed app>
-      <v-list dense>
-        <v-list-tile>
-          <nuxt-link to="/" class="navigation-link">
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                Главная
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </nuxt-link>
-        </v-list-tile>
-        <v-divider/>
-
-        <template v-for="item in items">
-          <!--  выпадающий список-->
-          <v-list-group
-            v-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children" :key="i">
-              <nuxt-link :to="child.src" class="navigation-link">
-                <v-list-tile-action v-if="child.icon">
-                  <v-icon>{{ child.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    {{ child.text }}
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </nuxt-link>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else :key="item.text">
-            <nuxt-link :to="item.src" class="navigation-link">
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </nuxt-link>
-          </v-list-tile>
-        </template>
-      </v-list>
+        <navbar-list></navbar-list>
     </v-navigation-drawer>
 
     <v-toolbar clipped-left color="primary" dark="" fixed app>
@@ -134,102 +77,17 @@
 </template>
 
 <script>
+  import NavbarList from "../components/NavbarList"
   export default {
+    components:{
+      NavbarList
+    },
     data() {
       return {
         clipped: true,
         drawer: null,
         fixed: false,
-        items: [
-          {
-            icon: 'keyboard_arrow_up',
-            'icon-alt': 'notifications',
-            text: 'Заявки',
-            model: false,
-            children: [
-              {icon: 'add', text: 'Новые заявки', src: {name: 'requests'}},
-              {icon: 'add', text: 'Все брони', src: {name: 'reservations'}},
-            ]
-          },
-          {icon: 'attach_money', text: 'Цены на номера', src: {name: 'prices'}},
-          {icon: 'list', text: 'Очередь', src: {name: "queue"}},
-          {
-            icon: 'keyboard_arrow_up',
-            'icon-alt': 'calendar_view_day',
-            text: 'Отчеты',
-            model: false,
-            children: [
-              {
-                icon: 'directions_car',
-                text: 'Трансфер',
-                src: {
-                  name: 'transfer-begin_date-end_date',
-                  params: {begin_date: this.currentYear + '-01-01', end_date: this.currentDateISO} // Переход на текущий календарный год
-                }
-              },
-              {
-                icon: 'local_parking',
-                text: 'Паркоместа',
-                src: {
-                  name: 'parking-begin_date-end_date',
-                  params: {
-                    begin_date: this.currentYear + '-01-01',
-                    end_date: this.currentDateISO
-                  } // Переход на текущий календарный год
-                }
-              },
-              {
-                icon: 'donut_large',
-                text: 'Статистика предоплат',
-                src: {
-                  name: 'prepaid-begin_date-end_date',
-                  params: {
-                    begin_date: this.currentYear + '-01-01',
-                    end_date: this.currentDateISO
-                  } // Переход на текущий календарный год
-                }
-              },
-              {
-                icon: 'donut_small',
-                text: 'Статистика оплат',
-                src: {
-                  name: 'payment-begin_date-end_date',
-                  params: {
-                    begin_date: this.currentYear + '-01-01',
-                    end_date: this.currentDateISO
-                  } // Переход на текущий календарный год
-                }
-              },
-              {
-                icon: 'supervised_user_circle',
-                text: 'Клиенты',
-                src: {
-                  name: 'clients-page',
-                  params: {
-                    page: 1
-                  }
-                }
-              }
-            ]
-          },
-          {icon: 'mail_outline', text: 'Рассылка писем', src: {name: 'messages'}},
-          {
-            icon: 'keyboard_arrow_up',
-            'icon-alt': 'settings_applications',
-            text: 'Параметры приложения',
-            model: false,
-            children: [
-              {icon: 'person', text: 'Пользователи', src: {name: 'users'}},
-              {icon: 'local_grocery_store', text: 'Тип оплаты', src: {name: 'payment_types'}},
-              {icon: 'home', text: 'Тип номеров', src: {name: 'room_types'}},
-              {icon: 'done_all', text: 'Статус номеров', src: {name: 'room_statuses'}},
-              {icon: 'local_hotel', text: 'Все номера', src: {name: 'rooms'}},
-              {icon: 'backup', text: 'Восстановление данных', src: {name: 'backup'}},
-            ]
-          },
-          {icon: 'settings', text: 'Настройки', src: {name: 'settings'}},
-          {icon: 'exit_to_app', text: 'Выйти', src: {name: 'logout'}}
-        ],
+
         miniVariant: false,
         right: true,
         rightDrawer: false,
@@ -240,10 +98,7 @@
           'Alabama',
 
         ],
-        currentYear: null,
-        currentMonth: null,
-        currentDay: null,
-        currentDateISO: null,
+
 
         snackbar: {
           state: null,
@@ -278,6 +133,8 @@
     },
 
     created() {
+
+
       // fixme
       // middleware
       var token = this.$cookies.get('token');
