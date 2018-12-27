8f9002b9d829e3a3cb90301daecc0f0692ab2048
voitsekhovskymax <voitsekhovskymax@gmail.com>
2018-12-27 18:30:11 +0200

    отчет
    - доделал отправку писем
    - начал страницу "Пользователи"

--- pages/messages/index.vue
+++ pages/messages/index.vue
@@ -32,60 +32,42 @@
             <v-radio label="Всем клиентам отеля" value="all"></v-radio>
           </v-radio-group>
           <template v-if="sendTo == 'single'">
-          <v-autocomplete v-model="search" :items="results" :loading="isLoading" :search-input.sync="keywords"
-                          return-object
-                          item-text="name" label="Найти клиента.." solo @change="activateClientDialog">
-            <template slot="no-data">
-              <v-list-tile>
-                <v-list-tile-title>
-                  Минимум <strong>три</strong> символа
-                </v-list-tile-title>
-              </v-list-tile>
-            </template>
-            <template
-              slot="item"
-              slot-scope="{ item, tile }"
-            >
-
-              <v-list-tile-avatar
-                color="success"
-                class="headline font-weight-light white--text"
+            <v-autocomplete v-model="search" :items="results" :loading="isLoading" :search-input.sync="keywords"
+                            return-object
+                            item-text="name" label="Найти клиента.." solo @change="activateClientDialog">
+              <template slot="no-data">
+                <v-list-tile>
+                  <v-list-tile-title>
+                    Минимум <strong>три</strong> символа
+                  </v-list-tile-title>
+                </v-list-tile>
+              </template>
+              <template
+                slot="item"
+                slot-scope="{ item, tile }"
               >
-                {{ item.name.charAt(0) }}
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.name"></v-list-tile-title>
-                <v-list-tile-sub-title><span v-if="item.email">{{item.email}} &nbsp &nbsp</span>{{item.phone}}
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
-            </template>
-          </v-autocomplete>
 
-          <v-text-field
-            v-model="request.email_client"
-            label="Email клиента"
-            required
-            hint="На этот email будет отослано письмо"
-          ></v-text-field>
+                <v-list-tile-avatar
+                  color="success"
+                  class="headline font-weight-light white--text"
+                >
+                  {{ item.name.charAt(0) }}
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-text="item.name"></v-list-tile-title>
+                  <v-list-tile-sub-title><span v-if="item.email">{{item.email}} &nbsp &nbsp</span>{{item.phone}}
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+              </template>
+            </v-autocomplete>
 
-          <v-select
-            label="Название письма"
-            hint="Выберите письмо из списка, и его содержимое добавится"
-            v-model="mail_select"
-            :items="messages"
-            item-text="title"
-            return-object
+            <v-text-field
+              v-model="request.email_client"
+              label="Email клиента"
+              required
+              hint="На этот email будет отослано письмо"
+            ></v-text-field>
 
-          ></v-select>
-
-          <v-textarea
-            v-model="mail_select.info"
-            auto-grow
-            label="Содержимое письма"
-            rows="1"
-          ></v-textarea>
-          </template>
-          <template v-else>
             <v-select
               label="Название письма"
               hint="Выберите письмо из списка, и его содержимое добавится"
@@ -103,10 +85,35 @@
               rows="1"
             ></v-textarea>
           </template>
+          <template v-else>
+            <v-alert :value="true" color="info" icon="info" outline class="mb-3">
+              Письмо будет отправлено всем клиентам, кроме клиентов из черного списка.
+            </v-alert>
+
+
+            <v-select
+              label="Выберите письмо"
+              hint="Выберите письмо из списка, и его содержимое добавится"
+              v-model="mail_select"
+              :items="messages"
+              item-text="title"
+              return-object
+
+            ></v-select>
+
+            <v-textarea
+              v-model="mail_select.info"
+              auto-grow
+              label="Содержимое письма"
+              rows="1"
+            ></v-textarea>
+          </template>
         </v-card-text>
         <v-card-actions>
-          <v-btn  v-if="sendTo == 'single'" color="success" @click="sendMail">Отправить клиенту {{request.email_client}}</v-btn>
-          <v-btn  v-else color="success" @click="sendMail">Отправить всем клиентам</v-btn>
+          <v-btn v-if="sendTo == 'single'" color="success" @click="sendMail">Отправить клиенту
+            {{request.email_client}}
+          </v-btn>
+          <v-btn v-else color="success" @click="sendMail">Отправить всем клиентам</v-btn>
 
         </v-card-actions>
       </v-card>
@@ -205,7 +212,14 @@
       return {
         progress: false,
         sendTo: 'single',
-        search: '',
+        search: {
+          name: null,
+          passport: null,
+          phone: null,
+          email: null,
+          address: null,
+          info: null,
+        },
         results: [],
         keywords: null,
         isLoading: null,
@@ -262,7 +276,47 @@
 
       },
       sendMail() {
+        this.progress = true;
         this.request.email_content = this.mail_select.info;
+        if (this.sendTo == 'single') {
+          this.axios.post('send-single-email', this.request).then((response) => {
+            console.log(response);
+            this.$notify({
+              group: 'global',
+              type: 'success',
+              title: 'Успешно',
+              text: 'Письмо отправлено',
+            });
+            this.progress = false;
+          }).catch((error) => {
+            console.log(error.response);
+            this.$notify({
+              group: 'global',
+              type: 'error',
+              title: 'Ошибка',
+              text: error.response,
+            });
+          })
+        } else {
+          this.axios.post('send-multiple-email', this.request).then((response) => {
+            console.log(response);
+            this.$notify({
+              group: 'global',
+              type: 'success',
+              title: 'Успешно',
+              text: 'Письма отправлены всем клиентам',
+            });
+            this.progress = false;
+          }).catch((error) => {
+            console.log(error.response);
+            this.$notify({
+              group: 'global',
+              type: 'error',
+              title: 'Ошибка',
+              text: error.response,
+            });
+          })
+        }
       }
     }
   };
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,115 +1,231 @@
 <template>
-    <div class="elevation-3">
-        <v-card>
-            <v-layout wrap>
-                <v-flex sm6 md6 xs12>
-                    <v-toolbar class="no-shadow">
-                        <v-toolbar-title>Пользователи</v-toolbar-title>
-                        <v-spacer></v-spacer>
-                    </v-toolbar>
-                </v-flex>
+  <div class="elevation-3">
+    <v-card>
+      <v-layout wrap>
+        <v-flex sm6 md6 xs12>
+          <v-toolbar class="no-shadow">
+            <v-toolbar-title>Пользователи</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </v-flex>
 
-                <v-flex sm6 md6 xs12>
-                    <v-toolbar class="no-shadow">
-                        <v-spacer></v-spacer>
-                        <v-btn icon>
-                            <v-icon>help_outline</v-icon>
-                        </v-btn>
-                    </v-toolbar>
-                </v-flex>
-            </v-layout>
-            <v-card-text>
-
-
-                <v-layout row wrap>
-                    <v-flex>
-                        <v-list three-line>
-                            <v-list-tile
-                                    v-for="user in response.users"
-                                    :key="user.id"
-                            >
+        <v-flex sm6 md6 xs12>
+          <v-toolbar class="no-shadow">
+            <v-spacer></v-spacer>
+            <v-btn color="success" @click="dialogNew=true">Новый пользователь</v-btn>
+            <v-btn icon>
+              <v-icon>help_outline</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </v-flex>
+      </v-layout>
+      <v-card-text>
 
 
-                                <v-list-tile-content>
-                                    <v-list-tile-title>{{ user.name }}</v-list-tile-title>
-                                    <v-list-tile-sub-title>{{ user.email }}</v-list-tile-sub-title>
-                                    <v-list-tile-sub-title>{{ getUserRole(user.role_id) }}</v-list-tile-sub-title>
-                                </v-list-tile-content>
+        <v-layout row wrap>
+          <v-flex>
+            <v-list three-line>
+              <v-list-tile
+                v-for="user in users"
+                :key="user.id"
+              >
 
 
-                                <v-list-tile-action>
-                                    <v-tooltip top>
-                                        <v-btn icon ripple slot="activator" @click="activeModalEdit(user)">
-                                            <v-icon color="green lighten-1">edit</v-icon>
-                                        </v-btn>
-                                        <span>Редактировать</span>
-                                    </v-tooltip>
-                                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ user.name }}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{ user.email }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{ getUserRole(user.role_id) }}</v-list-tile-sub-title>
+                </v-list-tile-content>
 
 
-                                <v-list-tile-action>
-                                    <v-tooltip top>
-                                        <v-btn icon ripple slot="activator">
-                                            <v-icon color="grey lighten-1">delete</v-icon>
-                                        </v-btn>
-                                        <span>Удалить</span>
-                                    </v-tooltip>
-                                </v-list-tile-action>
-                            </v-list-tile>
-                        </v-list>
-                    </v-flex>
-                </v-layout>
-            </v-card-text>
-        </v-card>
-
-        <v-dialog v-model="editDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-            <v-text-field label="Логин *" v-model="edit_user.name" required></v-text-field>
-            <v-text-field label="Email *" v-model="edit_user.email" required></v-text-field>
-            <v-select label="Роль" v-model="edit_user.role_id" :items="response.roles"  item-text="name"  item-value="id"></v-select>
-            <v-text-field label="Пароль *" type="password" hint="Введите здесь новый пароль для пользователя" required></v-text-field>
+                <v-list-tile-action>
+                  <v-tooltip top>
+                    <v-btn icon ripple slot="activator" @click="activeModalEdit(user)">
+                      <v-icon color="green lighten-1">edit</v-icon>
+                    </v-btn>
+                    <span>Редактировать</span>
+                  </v-tooltip>
+                </v-list-tile-action>
 
 
-        </v-dialog>
+                <v-list-tile-action>
+                  <v-tooltip top>
+                    <v-btn icon ripple slot="activator">
+                      <v-icon color="grey lighten-1">delete</v-icon>
+                    </v-btn>
+                    <span>Удалить</span>
+                  </v-tooltip>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
 
-    </div>
+    <v-dialog v-model="dialogNew" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Новый пользователь</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field label="ФИО пользоввателя" v-model="newUser.title"></v-text-field>
+          <v-text-field label="Пароль" v-model="newUser.password"></v-text-field>
+          <v-text-field label="Email" v-model="newUser.email"></v-text-field>
+
+          <v-select
+            label="Роль пользователя"
+            v-model="newUser.role_id"
+            :items="roles"
+            item-text="name"
+            item-value="id"
+            return-object
+          ></v-select>
+
+
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="success" @click="saveNewUser">Сохранить</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="dialogNew = false">Закрыть</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogEdit" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Редактирование письма</span>
+        </v-card-title>
+        <v-card-text>
+
+          <v-text-field label="Заголовок письма" v-model="editUser.title"></v-text-field>
+
+          <v-textarea
+            v-model="editUser.info"
+            auto-grow
+            label="Содержимое письма"
+            rows="1"
+          ></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="success" @click="saveEditUser">Обновить</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="dialogEdit = false">Закрыть</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogDelete" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Вы действительно хотите удалить это письмо?</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field label="Заголовок письма" v-model="deleteUser.title"></v-text-field>
+          <v-textarea
+            v-model="deleteUser.info"
+            auto-grow
+            label="Содержимое письма"
+            rows="1"
+          ></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="error" @click="saveDeleteUser">Удалить</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="dialogDelete = false">Закрыть</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+  </div>
 </template>
 
 <script>
 
-    export default {
-        name: 'users',
-        data() {
-            return {
-                //data
-                response: {},
-                //    helpers
-                edit_dialog: false,
-                edit_user: {},
-            };
+  export default {
+    name: 'users',
+    data() {
+      return {
+        //data
+        users: [],
+        roles: [],
+        //    helpers
+        editUser: {
+          name: null,
+          email: null,
+          password: null,
+          role_id: null
         },
-        beforeMount: function () {
+        deleteUser: {
+          name: null,
+          email: null,
+          password: null,
+          role_id: null
         },
-        mounted() {
-            this.axios.get('users').then((response) => {
-                this.response = response.data;
-            });
+        newUser: {
+          name: null,
+          email: null,
+          password: null,
+          role_id: null
         },
-        methods: {
-            activeModalEdit(user) {
-                this.edit_dialog = true;
-                this.edit_user = user;
+        dialogEdit: false,
+        dialogDelete: false,
+        dialogNew: false,
+      };
+    },
+    beforeMount: function () {
+    },
+    mounted() {
+      this.getUsers();
+    },
+    methods: {
+      getUsers() {
+        this.axios.get('users').then((response) => {
+          this.users = response.data.users;
+          this.roles = response.data.roles;
+        });
+      },
+      saveNewUser() {
+        this.axios.get('users').then((response) => {
+          this.$notify({
+            group: 'global',
+            type: 'success',
+            title: 'Успешно',
+            text: 'Новый пользователь создан',
+          });
+          this.getUsers();
+          this.dialogNew = false;
+        }).catch((error) => {
+          console.log(error.response);
+          this.$notify({
+            group: 'global',
+            type: 'error',
+            title: 'Ошибка',
+            text: error.response,
+          });
+        });
+      },
+      saveEditUser() {
+      },
+      saveDeleteUser() {
 
-            },
-            getUserRole(id) {
-                for (let i = 0; i < this.response.roles.length; i++) {
-                    if (this.response.roles[i].id == id) {
-                        return this.response.roles[i].name;
-                    }
-                }
-            }
+      },
+      activeModalEdit(user) {
+        this.edit_dialog = true;
+        this.edit_user = user;
 
+      },
+      getUserRole(id) {
+        for (let i = 0; i < this.roles.length; i++) {
+          if (this.roles[i].id == id) {
+            return this.roles[i].name;
+          }
         }
-    };
+      }
+
+    }
+  };
 </script>
 
 
